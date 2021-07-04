1223dfbc893fd64446e6c740ad8bc640c1a67dd4
Kenny Krosky <kenny.krosky@gmail.com>
2021-07-04 13:38:05 -0700

    update to placeholder

--- src/Components/AddToDos.js
+++ src/Components/AddToDos.js
@@ -36,7 +36,7 @@ const AddToDos = ({todos, setTodos}) => {
   return (
     <div className="add-to-do">
       <form onSubmit={(event) => add_to_do(event)} >
-        <input type="text" ref={to_do_value} placeholder="Take dog for a walk"/>
+        <input type="text" ref={to_do_value} placeholder="Add to do item"/>
         <button><svg  width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" ><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg></button>
       </form>
     </div>
