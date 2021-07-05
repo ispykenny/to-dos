c2c9709b1b607ea8e07b960c9387ca4349828244
Kenny Krosky <kenny.krosky@gmail.com>
2021-07-05 06:13:27 -0700

    accessibility fix

--- src/Components/AddToDos.js
+++ src/Components/AddToDos.js
@@ -37,7 +37,9 @@ const AddToDos = ({todos, setTodos}) => {
     <div className="add-to-do">
       <form onSubmit={(event) => add_to_do(event)} >
         <input type="text" ref={to_do_value} placeholder="Add to do item"/>
-        <button><svg  width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" ><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg></button>
+        <button>
+          <span className="sr-only">Add To Do Item</span>
+          <svg  width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" ><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg></button>
       </form>
     </div>
   )
