cbce22eb5daed1e11245688a730e7d730a8be01f
Kenny Krosky <kenny.krosky@gmail.com>
2021-07-05 10:40:10 -0700

    font size fix

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -9,7 +9,7 @@ const Nav = () => {
           <svg width="50" height="50" viewBox="0 0 140 140">
             <g transform="translate(-43 -17)">
               <rect width="140" height="140" rx="15" transform="translate(43 17)"/>
-              <text transform="translate(115 141)" fill="#fff" font-size="58" fontFamily="Lato-Bold, Lato" fontWeight="700"><tspan x="0" y="0">M</tspan></text>
+              <text transform="translate(115 141)" fill="#fff" fontSize="58" fontFamily="Lato-Bold, Lato" fontWeight="700"><tspan x="0" y="0">M</tspan></text>
             </g>
           </svg>
           <span>
